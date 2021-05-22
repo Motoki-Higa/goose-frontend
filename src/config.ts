113b9615ea0c048e65ec7a12a51bdb7946b9cac4
Motoki Higa <mokkyhiga@gmail.com>
2021-05-22 23:25:46 +0900

    feat: set conditional env variable for api url

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,11 @@
+let api = '';
+if (process.env.NODE_ENV === "production"){
+  api = 'https://goose-backend.herokuapp.com/api';
+} else {
+  api = 'http://localhost:5000/api';
+}
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    apiBaseUrl: 'http://localhost:5000/api',
+  apiBaseUrl: api,
 };
